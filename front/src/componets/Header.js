2ca629d6e5b9cd60107262d317886d938fffed53
ElizabethCT26 <mariaelizabethchuctun@gmail.com>
2024-02-01 10:51:37 -0500

    odio odio odio

---
+++ front/src/componets/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import tw from 'twrnc'
+
+function Header() {
+  return (
+    <div style={tw`bg-black h-20 w-20`}>
+      asasa
+    </div>
+  )
+}
+
+export default Header
